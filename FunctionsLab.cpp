faaa5aa17296661c3bf7e679334f9a4a8827a9d7
cs14af <alejandro.fernandez05@utrgv.edu>
2024-09-08 04:29:08 +0000

    FernandezAlejandro

--- FunctionsLab.cpp
+++ FunctionsLab.cpp
@@ -11,7 +11,22 @@ using namespace std; // making life easier so that we do not need to use std::ci
 
 
 int locateSmallest(int array[], int start, int end){
-	return -1; // fix this!
+	int minIndex= -1;
+	int minvalue=999;
+	for(int i = start ; i <= end;++i )
+	{
+		if (array[i] < minvalue)
+		{
+			
+			minIndex=i;
+			minvalue=array[i];
+
+		}
+
+
+}
+	return minIndex; // fix this!
+
 }
 
 //Write the function called replaceVariable in the given interval such that 
@@ -20,8 +35,15 @@ int locateSmallest(int array[], int start, int end){
 //     an int which is the starting index of search,
 //     an int which is the ending point of searchspace
 // function: Search in the array from given start to end index and replace the given target value with 1000
-void replaceVariable() {
-	return;
+ void replaceVariable(int array[], int start , int endmake,int target) 
+ {
+	for (int i = start;i<=endmake;i++)
+	{
+		if(array[i]==target)
+		{
+			array[i]=1000;
+		}
+	}
 }
 
 
@@ -29,8 +51,13 @@ void replaceVariable() {
 //write a function called printArray to print out the elements of the given array
 //output: nothing
 //input: the array of integers and its size
-void printArray() {
-	return;
+ void  printArray(int array[], int size) 
+  {
+	for(int i =0 ;i<size-1;i++){
+		cout<<array[i]<<" ";
+	}
+	
+	cout<<endl;
 }
 
 
@@ -49,5 +76,11 @@ void printArray() {
 
 
 int sumOdds(int array[], int start, int end){
-	return -1;
+	int sum =0;
+	for(int i = start; i<end;i++)
+	{
+		if(array[i]%2!=0)
+		sum+=array[i];
+	}
+	return sum;
 }
--- demo.cpp
+++ demo.cpp
@@ -2,5 +2,8 @@
 // run comman "make demo" to run this code
 
 int main() {
+    
+
     return 0;
+
 }
